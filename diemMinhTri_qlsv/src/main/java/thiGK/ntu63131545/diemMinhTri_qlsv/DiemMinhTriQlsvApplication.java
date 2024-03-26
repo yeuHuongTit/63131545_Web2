e2c81269c51631cc3d5b5e796e9f23d7497c5fde
chupedan <miinhtri1310@gmail.com>
2024-03-26 15:05:59 +0700

    push lan 1

---
+++ diemMinhTri_qlsv/src/main/java/thiGK/ntu63131545/diemMinhTri_qlsv/DiemMinhTriQlsvApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63131545.diemMinhTri_qlsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiemMinhTriQlsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiemMinhTriQlsvApplication.class, args);
+	}
+
+}
---
+++ diemMinhTri_qlsv/src/test/java/thiGK/ntu63131545/diemMinhTri_qlsv/DiemMinhTriQlsvApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63131545.diemMinhTri_qlsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiemMinhTriQlsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
