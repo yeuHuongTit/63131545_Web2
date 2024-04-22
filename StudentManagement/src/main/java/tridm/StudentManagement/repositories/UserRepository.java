2ed888533a8846231d3d4e4fd109db85d7e360dc
chupedan <miinhtri1310@gmail.com>
2024-04-22 23:06:55 +0700

    upload

---
+++ StudentManagement/src/main/java/tridm/StudentManagement/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package tridm.StudentManagement.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tridm.StudentManagement.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
