23d7722991cc96a5fbd3d3bc15f48483fd86efd6
chupedan <miinhtri1310@gmail.com>
2024-04-23 00:04:05 +0700

    upload

--- StudentManagement/src/main/java/tridm/StudentManagement/models/User.java
+++ StudentManagement/src/main/java/tridm/StudentManagement/models/User.java
@@ -1,12 +1,17 @@
 package tridm.StudentManagement.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;
