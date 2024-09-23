f8d0b537613ca474f81767014f81d1d1d3e30a99
chupedan <miinhtri1310@gmail.com>
2024-09-23 09:19:34 +0700

    thay email config

--- StudentManagement/src/main/java/tridm/StudentManagement/config/MailConfig.java
+++ StudentManagement/src/main/java/tridm/StudentManagement/config/MailConfig.java
@@ -13,17 +13,17 @@ public class MailConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com"); // Thay bằng host SMTP của bạn
+        mailSender.setHost("smtp.gmail.com"); 
         mailSender.setPort(587);
 
-        mailSender.setUsername("tri.dm.63cntt@ntu.edu.vn"); // Thay bằng email của bạn
-        mailSender.setPassword("scjrbfaogciwqryp"); // Thay bằng mật khẩu của bạn
+        mailSender.setUsername("tri.dm.63cntt@ntu.edu.vn"); 
+        mailSender.setPassword("cc"); // tạo mật khẩu ứng dụng (xác minh 2 bước trong gmail)
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true"); // Chỉ để debug, có thể bỏ qua trong môi trường production
+        props.put("mail.debug", "true"); 
 
         return mailSender;
     }
